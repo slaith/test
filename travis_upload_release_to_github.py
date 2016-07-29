5bfa3c1a56fa0f6c6670b0e0fc0dc67d902b22e6
prafull <prafull.singh@sourcefuse.com>
2016-07-29 14:24:37 +0530

    chanegs 3

--- travis_upload_release_to_github.py
+++ travis_upload_release_to_github.py
@@ -60,7 +60,7 @@ try:
 except CalledProcessError:
     current_tag_body = "Automatic upload for version %s" % current_tag
 
-github_access_token = getenv('GITHUB_ACCESS_TOKEN')
+github_access_token = 'f90b82656cbcbb28d8969637d9c7f4f388dc427e'
 
 if not github_access_token:
     print('No access token given, abort', file=sys.stderr)
